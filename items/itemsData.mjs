25a904d3092d6a68588864035ff52749c128bd87
Andrew Morrison <morrison.andrew422@gmail.com>
2024-03-05 14:00:51 -0500

    setup itemdata

---
+++ items/itemsData.mjs
@@ -0,0 +1,13 @@
+export default {
+  manaPotion: {
+    name: 'mana potion',
+    type: 'potion',
+    effect: [
+      { sustinaceBuff: 50 },
+    ],
+    appearance: {
+      color: 'red',
+      style: 'swirling',
+    },
+  },
+}
