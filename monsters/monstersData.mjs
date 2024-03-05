974c67544d816c619ec8b67918178308daee5350
venmorr <stevenmorrrca@email.com>
2024-03-05 16:40:14 -0500

    add wraith and update stats

--- monsters/monstersData.mjs
+++ monsters/monstersData.mjs
@@ -9,7 +9,7 @@ export default {
       skill: 70,
       stamina: 50,
       sorcery: 80,
-      substinance: 30,
+      substainance: 30,
       stealth: 40,
     },
     spawnableBiomes: [ 'swamp', 'forest', 'mountain top' ],
@@ -22,6 +22,32 @@ export default {
       color: 'green',
       size: 'medium',
       shape: 'hunched'
+    }
+  },
+
+  hag: {
+    name: 'Wraith',
+    stats: {
+      stench: 10,
+      strength: 65,
+      skill: 70,
+      stamina: 80,
+      sorcery: 80,
+      speech: 45,
+      substainance: 30,
+      stealth: 40,
+      spunk: 50
+    },
+    spawnableBiomes: [ 'sky', 'ruins', 'catacombs' ],
+    itemDrop: [ 
+      items.manaPotion,
+      // items.goldCoin,
+      // items.exp(10)
+    ],
+    appearence: {
+      color: 'black',
+      size: 'large',
+      shape: 'ghastly'
     }
   }
 }
